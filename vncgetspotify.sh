99753b6603218761f6a3d7aa69d94b104ad4b600
ksyasuda <ksyasuda@umich.edu>
2020-11-26 14:10:55 -0800

    Remove old unused scripts

--- vncgetspotify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# vncgetspotify.sh
-
-export DISPLAY=:0
-
-id=$(wmctrl -lx | awk '/spotify.exe.Wine/ {print $1}')
-[[ -z $id ]] && id=$(wmctrl -lx | awk '/spotify.Spotify/ {print $1}')
-
-x11vnc -sid $id -display :0 -auth .Xauthority
