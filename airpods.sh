29022f50d9c4f349e742daea681ce67e123a2bbb
ksyasuda <ksyasuda@umich.edu>
2020-11-26 14:09:10 -0800

    Update airpods script to output nothing by default
    -v to see the output from commands

--- airpods.sh
+++ airpods.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-(bluetoothctl connect 24:1B:7A:ED:ED:78)
+if [[ $# -gt 0 && $1 == "-v" ]]; then
+	bluetoothctl power on && bluetoothctl connect 24:1B:7A:ED:ED:78
+	exit 0
+fi
+bluetoothctl power on >/dev/null && bluetoothctl connect 24:1B:7A:ED:ED:78 >/dev/null
