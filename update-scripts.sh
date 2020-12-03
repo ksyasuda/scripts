3ea25a4b8bcdb3290bc188f59122d63e1ef9ce2d
ksyasuda <ksyasuda@umich.edu>
2020-12-02 17:08:48 -0800

    Automated udate of the scrips repo

--- update-scripts.sh
+++ update-scripts.sh
@@ -4,14 +4,14 @@ set -Euo pipefail
 
 DIR=~/scripts/
 
-if [[ $(git status | grep 'nothing to commit, working tree clean') != '' ]]
-then
-	echo 'Nothing to commit, repo updated'
-	exit 0
-fi
-
 (
 	cd $DIR
+	if [[ $(git status | grep 'nothing to commit, working tree clean') != '' ]]
+	then
+		echo 'Nothing to commit, repo updated'
+		exit 0
+	fi
+
 	git add .
 	git commit -m "Automated udate of the scrips repo"
 	git push
